772fd3c7e5c03416c16124d2259b9c9cb5f7f5e0
Aaron Powell <Apowell829@gmail.com>
2021-01-28 17:27:57 +0000

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,7 @@
 exports.handler = function(event, context, callback) {
-  console.log(event, context)
+  console.log(event, context) // Seen in netlify functions tab
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: `Hello, World. Your referer was: ${event.headers.referer}`
   });
 };
